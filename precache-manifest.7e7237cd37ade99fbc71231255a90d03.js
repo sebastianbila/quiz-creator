422b4410b3bd978111b484513a8bd47c84fa12b6
sebii_sw <bilasebastyan@gmail.com>
2020-10-10 12:30:08 +0300

    Updates

---
+++ precache-manifest.7e7237cd37ade99fbc71231255a90d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8784db54e2cdbfb590ac1f48bbdae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5577b52bf64d6f538b35",
+    "url": "/static/css/main.e3f056ff.chunk.css"
+  },
+  {
+    "revision": "a012296b2f04b4bf2856",
+    "url": "/static/js/2.b2cff404.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/static/js/2.b2cff404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5577b52bf64d6f538b35",
+    "url": "/static/js/main.a2030530.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  }
+]);
